86f7fdbfbf1573a5cac4abefcecb6db0a616d570
venkat-yalamanchili <venkatyalamanchili56@gmail.com>
2026-02-24 12:31:47 -0800

    delete node in BST

--- Binary search Tree/create.py
+++ Binary search Tree/create.py
@@ -56,8 +56,34 @@ def search(rootNode, nodeValue):
     else:
         return search(rootNode.rightchild, nodeValue)
     
+def minValueNode(bstNode): #this returns the min value uder the given node
+    current  = bstNode
+    while (current.leftchild): # we just check the left subtree because min values are always in left subtree
+        current = current.leftchild
+    return current
+
 def deleteNode(rootNode, nodeValue):
-    pass
+    if rootNode is None:
+        return rootNode
+    if nodeValue < rootNode.data:
+        rootNode.leftchild = deleteNode(rootNode.leftchild, nodeValue)
+    elif nodeValue > rootNode.data:
+        rootNode.rightchild = deleteNode(rootNode.rightchild, nodeValue)
+    else:
+        if rootNode.leftchild is None:
+            temp = rootNode.rightchild
+            rootNode = None
+            return temp
+        
+        if rootNode.rightchild is None:
+            temp = rootNode.leftchild
+            rootNode = None
+            return temp
+        temp = minValueNode(rootNode.rightchild)
+        rootNode.data = temp.data
+        rootNode.rightchild = deleteNode(rootNode.rightchild, temp.data)
+    return rootNode
+
 
 def deleteBST(rootNode):
     rootNode.data = None
@@ -70,6 +96,10 @@ newBST = BSTNode(None)
 insertNode(newBST,70)
 insertNode(newBST,60)
 insertNode(newBST,80)
-print(newBST.data , newBST.leftchild.data, newBST.rightchild.data)
+# print(newBST.data , newBST.leftchild.data, newBST.rightchild.data)
 levelorderTraversal(newBST)
-print(search(newBST, 40))
+# print(search(newBST, 40))
+
+deleteNode(newBST, 70)
+
+levelorderTraversal(newBST)

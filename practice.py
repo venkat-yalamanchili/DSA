87fec0b0118a5e4c03764c3fe392f6a02b4a1305
venkat-yalamanchili <venkatyalamanchili56@gmail.com>
2026-03-16 12:20:48 -0700

    practice

--- practice.py
+++ practice.py
@@ -1,5 +1,19 @@
-a = [1,2,3,5]
+def print_activites(activites):
+    activites.sort(key =lambda x:x[2])
+    print(activites[0][0])
+    last_finish_time = activites[0][2]
 
-x = a.pop(0)
+    for j in range (1, len(activites)):
+        if activites[j][1] >= last_finish_time:
+            print(activites[j][0])
+            last_finish_time = activites[j][2]
 
-print(type(x))
+activites = [["A1",0,6],
+             ["A2",3,4],
+             ["A3",1,2],
+             ["A4",5,8],
+             ["A5",5,7],
+             ["A6",8,9]
+             ]
+
+print_activites(activites)
